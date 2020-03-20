0ffebcf92414b627943300d885bfe3006ac746bf
Toka Elkeik <tokaelkeik@gmail.com>
2020-03-20 04:01:22 +0200

    O(n) runtime sloution with hard coded tests

---
+++ main/src/main/main.java
@@ -0,0 +1,33 @@
+package main;
+import java.io.*; 
+import java.util.HashMap;
+
+public class main {
+	public static void main(String[] args) {
+		boolean result = checkMapping("tee", "bar");
+		System.out.println(result);
+	}
+	public static boolean checkMapping(String s1, String s2) {
+		if(s1.length() != s2.length()) {
+			return false;
+		}
+		HashMap<Character, Character> map1 = new HashMap<Character, Character>();
+		HashMap<Character, Character> map2 = new HashMap<Character, Character>();
+		
+		for(int i = 0 ; i < s1.length() ; i++) {
+			char x = s1.charAt(i);
+			char y = s2.charAt(i);
+			
+			if(map1.containsKey(x) && y != map1.get(x) ) {
+					return false;	
+			}else {
+				if(map2.containsKey(y)) {
+					return false;
+				}
+				map1.put(x, y);
+				map2.put(y, x);
+			}
+		}		
+		return true;
+	}
+}
